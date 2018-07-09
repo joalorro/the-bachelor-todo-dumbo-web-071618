312de431a1c4f9f593876f0fb12da550a47cdd92
Josue V. Alorro <joalorro@gmail.com>
2018-07-09 17:07:28 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -37,7 +37,18 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0 
+
+  data.each do |season_number, contestant_array|
+  
+    contestant_array.each do |contestant|
+      
+      contestant.each do |category, info|
+        counter += 1 if info == hometown
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
